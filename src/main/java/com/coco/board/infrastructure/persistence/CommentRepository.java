14b704000fa8ac0451ac6368888cd3bd35805c37
SeeQ <sxdz1290@naver.com>
2023-11-02 00:46:08 +0900

    Initial commit

---
+++ src/main/java/com/coco/board/infrastructure/persistence/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.coco.board.infrastructure.persistence;
+
+import com.coco.board.domain.Comment;
+import com.coco.board.domain.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    /* 게시글 댓글 목록 가져오기 */
+    List<Comment> getCommentByPostsOrderById(Posts posts);
+
+    Optional<Comment> findByPostsIdAndId(Long postsId, Long id);
+}
